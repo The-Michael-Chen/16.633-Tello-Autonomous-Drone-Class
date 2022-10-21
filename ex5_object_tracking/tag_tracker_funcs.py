5ad829ea06e690a7233feb5065dff9e5da9d89ea
The-Michael-Chen <02chenmichael@gmail.com>
2022-10-21 16:38:25 -0400

    finished ex 5

--- ex5_object_tracking/tag_tracker_funcs.py
+++ ex5_object_tracking/tag_tracker_funcs.py
@@ -1,5 +1,3 @@
-# code partially adapted from https://pypi.org/project/pupil-apriltags/
-# and https://pyimagesearch.com/2020/11/02/apriltag-with-python/
 from djitellopy import Tello
 import cv2
 import time
@@ -74,64 +72,60 @@ def get_point_below_tag(T_camera_tag, meters_below, K):
     x_img = x_img[0:2, :]
     px = int(x_img[0][0])
     py = int(x_img[1][0])
-    print(px, py)
     return (px, py)
     
     ##################################
 
-def track_tags(img):
+def detect_tags(img, target_point_dist = None, visualize = False):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     tags = at_detector.detect(img_gray, estimate_tag_pose=True,
                               camera_params=intrinsics, tag_size=tag_size)
     print("number of tags detected:", len(tags))
-    
+    tag_info = {}
+    tag_num = 0
     # loop over the AprilTag detection results
     for tag in tags:
-        print("tag id:", tag.tag_id)
+        tag_num = tag_num + 1
+        target = None
     	# extract the bounding box (x, y)-coordinates for the AprilTag
     	# and convert each of the (x, y)-coordinate pairs to integers
-        (ptA, ptB, ptC, ptD) = tag.corners
-        ptB = (int(ptB[0]), int(ptB[1]))
-        ptC = (int(ptC[0]), int(ptC[1]))
-        ptD = (int(ptD[0]), int(ptD[1]))
-        ptA = (int(ptA[0]), int(ptA[1]))
-    	# draw the bounding box of the AprilTag detection
-        cv2.line(img, ptA, ptB, (0, 255, 0), 2)
-        cv2.line(img, ptB, ptC, (0, 255, 0), 2)
-        cv2.line(img, ptC, ptD, (0, 255, 0), 2)
-        cv2.line(img, ptD, ptA, (0, 255, 0), 2)
-    	# draw the center (x, y)-coordinates of the AprilTag
-        (cX, cY) = (int(tag.center[0]), int(tag.center[1]))
-        cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
-    	# draw the tag family on the image
-        tagFamily = tag.tag_family.decode("utf-8")
-        cv2.putText(img, tagFamily, (ptA[0], ptA[1] - 15),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        
-        print("[INFO] tag family: {}".format(tagFamily))
-        print("t: ", tag.pose_t)
-        
+        if visualize: 
+            (ptA, ptB, ptC, ptD) = tag.corners
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+            # draw the bounding box of the AprilTag detection
+            cv2.line(img, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(img, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(img, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(img, ptD, ptA, (0, 255, 0), 2)
+            # draw the center (x, y)-coordinates of the AprilTag
+            (cX, cY) = (int(tag.center[0]), int(tag.center[1]))
+            cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
+            # draw the tag family on the image
+            tagFamily = tag.tag_family.decode("utf-8")
+            cv2.putText(img, tagFamily, (ptA[0], ptA[1] - 15),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         ##### Detect point set distance below tag #####
         # construct 4x4 transform matrix of the tags location wrt the camera frame
-        T_camera_tag = np.zeros((4,4))
-        T_camera_tag[3,3] = 1.0
-        T_camera_tag[0:3,3:] = tag.pose_t
-        T_camera_tag[0:3,0:3] = tag.pose_R
-        
-        ###### TODO fill in here ##########
-        point_below = get_point_below_tag(T_camera_tag, below_meters, K=K)
-        
-        # # Draw line from tag center to (px,py) on img. Hint, use cv2.line
-        cv2.line(img, point_below, (cX, cY), (255, 0, 0) , 3)
-        ###################################
-        #get_point_below_tag(T_camera_tag, meters_below = 0.5, K = K)
-        
+        if target_point_dist != None:
+            T_camera_tag = np.zeros((4,4))
+            T_camera_tag[3,3] = 1.0
+            T_camera_tag[0:3,3:] = tag.pose_t
+            T_camera_tag[0:3,0:3] = tag.pose_R
+            point_below = get_point_below_tag(T_camera_tag, meters_below=0.5, K=K)
+            if visualize:
+                cv2.line(img, point_below, (cX, cY), (255, 0, 0) , 3)
+            target = {'position': T_camera_tag, 'pixel_coords': point_below}
+        tag_info[tag_num] = {tag.tag_id: tag, 'target': target}
+    if visualize:
         cv2.imshow("drone cam", img)
         key = cv2.waitKey(50)
+    return tag_info
 
 while True:
-    # GET THE IMAGE FROM TELLO
     frame_read = tello.get_frame_read()
     myFrame = frame_read.frame
     img = cv2.resize(myFrame, (width, height))
-    track_tags(img)
+    print(detect_tags(img, target_point_dist = 0.1, visualize = True))
